ae5b0a1c695e297180afe841e393d651bb61baee
podhmo <podhmo+altair@beproud.jp>
2012-12-15 16:52:23 +0900

    add simple index view

--- advent/advent/models.py
+++ advent/advent/models.py
@@ -1,28 +1,24 @@
-from sqlalchemy import (
-    Column,
-    Integer,
-    Text,
-    )
-
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
 from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-    )
-
 from zope.sqlalchemy import ZopeTransactionExtension
-
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+    
+DBSession = orm.scoped_session(orm.sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+class Group(Base):
+    __tablename__ = "groups"
+    query = DBSession.query_property()
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.Unicode(255))
 
+class Student(Base):
+    __tablename__ = "students"
+    query = DBSession.query_property()
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.Unicode(255))
+    grade = sa.Column(sa.Integer)
+    group_id = sa.Column(sa.Integer, sa.ForeignKey("groups.id"))
+    group = orm.relationship("Group", uselist=False, backref="students")
---
+++ advent/advent/templates/index.mako
@@ -0,0 +1,9 @@
+<h2> students list</h2>>
+
+<table>
+  <tbody>
+  %for student in students:
+    <tr><td>${student.name}</td></tr>
+  %endfor
+  </tbody>
+</table>

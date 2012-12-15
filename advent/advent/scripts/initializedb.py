201e7da7b729a919315a4e55cd2e82042e4753d6
podhmo <podhmo+altair@beproud.jp>
2012-12-15 17:14:54 +0900

    tiny fixture

---
+++ advent/advent/scripts/initializedb.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import transaction
+
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import (
+    get_appsettings,
+    setup_logging,
+    )
+
+from ..models import Base, DBSession
+from ..models import Group, Student
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri>\n'
+          '(example: "%s development.ini")' % (cmd, cmd)) 
+    sys.exit(1)
+
+def main(argv=sys.argv):
+    if len(argv) != 2:
+        usage(argv)
+    config_uri = argv[1]
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri)
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        do_initialize()
+
+def do_initialize():
+    g = Group(name=u"Gryffindor")
+    h = Group(name=u"Hufflepuff")
+    r = Group(name=u"Ravenclaw")
+    s = Group(name=u"Slytherin")
+
+    g.students.append(Student(name=u"aaa", grade=1))
+    g.students.append(Student(name=u"bbb", grade=1))
+    g.students.append(Student(name=u"ccc", grade=1))
+
+    h.students.append(Student(name=u"hhh", grade=1))
+    h.students.append(Student(name=u"iiii", grade=1))
+
+    r.students.append(Student(name=u"rrr", grade=1))
+    r.students.append(Student(name=u"ssss", grade=1))
+    r.students.append(Student(name=u"tttttt", grade=1))
+    r.students.append(Student(name=u"uu", grade=1))
+
+    s.students.append(Student(name=u"xxxx", grade=1))
+    s.students.append(Student(name=u"yyy", grade=1))
+    s.students.append(Student(name=u"z", grade=1))
+
+    DBSession.add_all([g, h, r, s])
+
